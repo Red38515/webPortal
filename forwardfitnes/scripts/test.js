0ce0ecc3b264f4fa1be1d4527e3cd991de6926c9
Steeve <114946262+Red38515@users.noreply.github.com>
2023-02-03 17:57:36 +0000

    Uploaded files for completed projects

---
+++ forwardfitnes/scripts/test.js
@@ -0,0 +1,8 @@
+function day() {
+	var day = document.getElementById("date");
+	if (day === "saturday") {
+		document.write("I love Saturdays");
+	} else {
+		document.write("I wish it was Saturday")
+	}
+}
